d8cd720e4e2f4fe884bb49127490769d0e4d715d
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2021-01-15 19:25:53 +0530

    Update Server.java

--- Server.java
+++ Server.java
@@ -19,23 +19,44 @@ public class Server extends JFrame {
         l1.setBounds(5, 17, 30, 30);
         p1.add(l1);// add to panel
 
-        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons\\1.png"));// loading arrow image
+        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons\\1.png"));// loading profile image
         Image i5 = i4.getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT);// scaling it
         ImageIcon i6 = new ImageIcon(i5);// loading the scaled image
         JLabel l2 = new JLabel(i6);// adding it to label
         l2.setBounds(40, 5, 60, 60);
         p1.add(l2);// add to panel
 
-        JLabel l3 = new JLabel("Gaitonde");//adding another label
-        l3.setFont(new Font("SAN_SERIF",Font.BOLD,18));
+        ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icons\\video.png")); // loading video icon
+        Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);// scaling it
+        ImageIcon i9 = new ImageIcon(i8);// loading the scaled image
+        JLabel l5 = new JLabel(i9);// adding it to label
+        l5.setBounds(290, 20, 30, 30);
+        p1.add(l5);// add to panel
+
+        ImageIcon i11 = new ImageIcon(ClassLoader.getSystemResource("icons\\phone.png")); // loading phone icon
+        Image i12 = i11.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);// scaling it
+        ImageIcon i13 = new ImageIcon(i12);// loading the scaled image
+        JLabel l6 = new JLabel(i13);// adding it to label
+        l6.setBounds(350, 20, 35, 30);
+        p1.add(l6);// add to panel
+
+        ImageIcon i14 = new ImageIcon(ClassLoader.getSystemResource("icons\\3icon.png")); // loading buttons icon
+        Image i15 = i14.getImage().getScaledInstance(13, 25, Image.SCALE_DEFAULT);// scaling it
+        ImageIcon i16 = new ImageIcon(i15);// loading the scaled image
+        JLabel l7 = new JLabel(i16);// adding it to label
+        l7.setBounds(410, 20, 13, 25);
+        p1.add(l7);// add to panel
+
+        JLabel l3 = new JLabel("Gaitonde");// adding another label
+        l3.setFont(new Font("SAN_SERIF", Font.BOLD, 18));
         l3.setForeground(Color.WHITE);
-        l3.setBounds(110,15,100,18);
+        l3.setBounds(110, 15, 100, 18);
         p1.add(l3);
 
         JLabel l4 = new JLabel("Active Now");
-        l4.setFont(new Font("SAN_SERIF",Font.PLAIN,14));
+        l4.setFont(new Font("SAN_SERIF", Font.PLAIN, 14));
         l4.setForeground(Color.WHITE);
-        l4.setBounds(115,35,100,20);
+        l4.setBounds(115, 35, 100, 20);
         p1.add(l4);
 
         setLayout(null);
